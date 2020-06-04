ce26d2f5d840437558050e0ab8b80f9bcd0158ec
Vojtěch Michal <vojta.michall@email.cz>
2020-06-04 15:08:44 +0200

    Make screen resolution configurable.

--- snake.c
+++ snake.c
@@ -8,21 +8,27 @@
 
 #include "peripherals.h"
 
+#define WIDTH 480
+#define HEIGHT 320
+#define SIDE 8
+
+#define COLUMNS (WIDTH / SIDE)
+#define ROWS (HEIGHT / SIDE)
 
 int main(int argc, char *argv[]) {
-	int board[48 * 32 * 2 *2];
+	int board[COLUMNS * ROWS];
 
 	bool som = false;
-	for(int y = 0; y < 32 * 2; ++y) {
-		for(int i = 0; i < 48 * 2; ++i) {
-			board[y * 48 * 2+ i] = som;
+	for(int y = 0; y < ROWS; ++y) {
+		for(int i = 0; i < COLUMNS; ++i) {
+			board[y * COLUMNS + i] = som;
 			som = !som;
 		}
 		som = !som;
 	}
 	
 	peripherals_intit();
-	draw_board(board, 48 * 2, 32 * 2, 5);
+	draw_board(board, COLUMNS, ROWS, SIDE);
 
 	printf("Goodbye world\n");
 
