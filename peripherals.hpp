18e318fa74d2a4943dd115d02e7f09ce009c43e7
Josef Zelinka <zelinjo1@fel.cvut.cz>
2020-06-11 22:03:11 +0200

    Header fixed.

--- peripherals.hpp
+++ peripherals.hpp
@@ -12,6 +12,12 @@ extern "C" {
 #endif
 void peripherals_init();
 
+void fill_pixel_lcd(int pos_x, int pos_y, unsigned short color);
+
+void fill_square_lcd(int col, int row, unsigned short color);
+
+void flood_fill_lcd(unsigned short color);
+
 void draw_board(int *board, int board_w, int board_h, int cell_s);
 
 void color_square(int col, int row, unsigned short color, unsigned short *window);
