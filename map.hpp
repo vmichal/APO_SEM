be182456e2833350f9a5d2f3ec60a54b2323a6a4
Vojtěch Michal <vojta.michall@email.cz>
2020-06-13 20:45:49 +0200

    Hotfix: add map.cpp|hpp to git.

---
+++ map.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+
+namespace game {
+
+	struct coord {
+		int x, y;
+	};
+
+
+	inline constexpr coord operator+(coord const a, coord const b) {
+		return coord{ a.x + b.x, a.y + b.y };
+	}
+
+	inline constexpr bool operator==(coord const a, coord const b) {
+		return a.x == b.x && a.y == b.y;
+	}
+
+	inline constexpr int modulo_value(int value, int modulo) {
+		if (value < 0) return value + modulo;
+		return value >= modulo ? value - modulo : value;
+	}
+
+	inline constexpr coord coord_clamp(coord value, coord max) {
+		return coord{ modulo_value(value.x, max.x), modulo_value(value.y, max.y) };
+	}
+
+
+	enum class Entity {
+		snake, wall, food, none
+	};
+
+	enum class Direction {
+		north, south, west, east
+	};
+
+	coord displacement_in_direction(Direction);
+	Direction turn_left(Direction);
+	Direction turn_right(Direction);
+	Direction opposite_direction(Direction);
+	char const* to_string(Direction d);
+
+	struct Square {
+		coord position_;
+		Entity entity_;
+	};
+
+	class Map {
+
+		std::vector<std::vector<Square>> game_board_;
+		coord const size_;
+
+		static Map load_from_file(std::string file);
+
+	public:
+		static void load_maps(std::string directory);
+
+		Map(int width, int height)
+			: size_{ width, height }, game_board_(height, std::vector<Square>(width)) {}
+
+		void draw() const;
+		coord size() const { return size_; }
+		std::vector<std::vector<Square>>& board() { return game_board_; }
+
+	};
+
+
+}
+
+
